c8b004dc501092adc5e98c14bc6d5793d6a44254
tayutaedomo <tayutaedomo@gmail.com>
2018-11-23 20:00:49 +0900

    Add trial codes of web samples

---
+++ Assets/Scripts/MyWebView.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * Refer: https://qiita.com/snaka/items/46042be015ad320d5f4b
+ */
+public class MyWebView : MonoBehaviour {
+
+    // Inspector で画面上に配置した GUI Text にアタッチする
+    public Text DebugText;
+
+    string m_filePath;
+
+    // Use this for initialization
+    void Start () {
+        m_filePath = Path.Combine(Application.persistentDataPath, "sample.html");
+        Debug.Log (m_filePath);
+        PrepareHTML();
+
+        // このスクリプトがアタッチされているGameObjectからWebViewObjectを取得する
+        var webview = GetComponent<WebViewObject>();
+
+        // WebViewObject の初期化時にWebページ側から呼び出すことができるコールバック関数を定義する。
+        // Web側からコールバック関数呼び出すには、リンク要素の href 属性などURLを指定する箇所で
+        // 'unit:(任意の文字列)' のように指定すると、コールバック関数が呼び出される。
+        // このとき、"(任意の文字列)"の部分が関数の引数として渡される。
+        webview.Init ((string msg) => {
+            Debug.Log ("Call from Web view : " + msg);
+            DebugText.text = msg;
+        });
+
+        var url = "file://" + m_filePath;
+        // var url = "https://google.co.jp";
+        Debug.Log(url);
+
+        webview.LoadURL(url);
+        webview.SetVisibility(true);
+        webview.SetMargins(10,100,10,10);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    }
+
+    void PrepareHTML() {
+        using(var writer = new StreamWriter(m_filePath, false)) {
+            writer.Write(
+@"<html>
+<body>
+Hello unity-webview !!!<br/>
+<ul>
+  <li><a href='unity:hoge'>callback 'hoge'</a></li>
+  <li><a href='unity:fuga'>callback 'fuga'</a></li>
+</ul>
+</body>
+</html>
+");
+            writer.Close();
+        }
+    }
+}
---
+++ Assets/Scripts/SimpleWebView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Refer: https://qiita.com/mczkzk/items/0ed0665c7da3b550770e#unity-webview
+ */
+public class SimpleWebView : MonoBehaviour {
+
+    private string url = "https://www.google.co.jp/";
+    WebViewObject webViewObject;
+
+    // Use this for initialization
+    void Start () {
+        webViewObject = (new GameObject("WebViewObject")).AddComponent<WebViewObject>();
+        webViewObject.Init((msg) => {
+            Debug.Log(msg);
+        });
+        webViewObject.LoadURL(url);
+        // 中央に配置
+        Debug.Log("Screen.width: " + Screen.width);
+        Debug.Log("Screen.height: " + Screen.height);
+        webViewObject.SetMargins(Screen.width / 4, Screen.height / 4, Screen.width / 4, Screen.height / 4);
+        webViewObject.SetVisibility(true);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    }
+}
